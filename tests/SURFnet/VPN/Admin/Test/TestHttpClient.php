33f70fa6d01e4401539498cb3a7a31694a18858d
François Kooman <fkooman@tuxed.net>
2016-09-27 10:05:17 +0200

    add initial tests

---
+++ tests/SURFnet/VPN/Admin/Test/TestHttpClient.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ *  Copyright (C) 2016 SURFnet.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace SURFnet\VPN\Portal\Test;
+
+use SURFnet\VPN\Common\HttpClient\HttpClientInterface;
+use RuntimeException;
+
+class TestHttpClient implements HttpClientInterface
+{
+    public function get($requestUri)
+    {
+        switch ($requestUri) {
+            case 'serverClient/server_pools':
+                return self::wrap(
+                    'server_pools',
+                    [
+                        'internet' => [
+                            'enableAcl' => false,
+                            'displayName' => 'Internet Access',
+                            'twoFactor' => false,
+                            'processCount' => 4,
+                            'hostName' => 'vpn.example',
+                        ],
+                    ]
+                );
+            case 'serverClient/client_connections':
+                return self::wrap(
+                    'client_connections',
+                    [
+                        'connections' => [
+                            [
+                                'bytes_in' => 5428,
+                                'bytes_out' => 5504,
+                                'common_name' => 'me_test',
+                                'connected_since' => 1474963126,
+                                'name' => 'test',
+                                'real_address' => '192.168.122.1:55461',
+                                'user_id' => 'me',
+                                'virtual_address' => [
+                                    '10.111.138.2',
+                                    'fd53:dbd:11bf:6460::1000',
+                                ],
+                            ],
+                        ],
+                        'id' => 'internet',
+                    ]
+                );
+
+//            case 'serverClient/server_pool?pool_id=internet':
+//                return self::wrap(
+//                    'server_pool',
+//                    [
+//                        'enableAcl' => false,
+//                        'displayName' => 'Internet Access',
+//                        'twoFactor' => false,
+//                        'processCount' => 4,
+//                        'hostName' => 'vpn.example',
+//                    ]
+//                );
+//            case 'serverClient/has_otp_secret?user_id=foo':
+//                return self::wrap('has_otp_secret', false);
+//            case 'caClient/user_certificate_list?user_id=foo':
+//                return self::wrap('user_certificate_list', [['name' => 'FooConfig', 'user_id' => 'foo', 'state' => 'V']]);
+//            case 'serverClient/user_groups?user_id=foo':
+//                return self::wrap('user_groups', []);
+//            case 'serverClient/disabled_common_names':
+//                return self::wrap('disabled_common_names', []);
+            default:
+                throw new RuntimeException(sprintf('unexpected requestUri "%s"', $requestUri));
+        }
+    }
+
+    public function post($requestUri, array $postData)
+    {
+        switch ($requestUri) {
+//            case 'caClient/add_client_certificate':
+//                return self::wrap(
+//                    'add_client_certificate',
+//                    [
+//                        'cn' => 'foo_MyConfig',
+//                        'valid_from' => 12345678,
+//                        'valid_to' => '23456789',
+//                        'ca' => 'CAPEM',
+//                        'cert' => 'CERTPEM',
+//                        'key' => 'KEYPEM',
+//                        'ta' => 'TAKEY',
+//                    ]
+//                );
+//            case 'serverClient/disable_common_name':
+//                return self::wrap('disable_common_name', true);
+//            case 'serverClient/kill_client':
+//                return self::wrap('kill_client', true);
+//            case 'serverClient/set_voot_token':
+//                return self::wrap('set_voot_token', true);
+            default:
+                throw new RuntimeException(sprintf('unexpected requestUri "%s"', $requestUri));
+        }
+    }
+
+    private static function wrap($key, $response)
+    {
+        return [
+            'data' => [
+                $key => $response,
+            ],
+        ];
+    }
+}
---
+++ tests/SURFnet/VPN/Admin/VpnAdminModuleTest.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *  Copyright (C) 2016 SURFnet.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace SURFnet\VPN\Admin;
+
+require_once sprintf('%s/Test/JsonTpl.php', __DIR__);
+require_once sprintf('%s/Test/TestHttpClient.php', __DIR__);
+
+use SURFnet\VPN\Common\Http\NullAuthenticationHook;
+use SURFnet\VPN\Common\Http\Service;
+use SURFnet\VPN\Common\Http\Request;
+use SURFnet\VPN\Common\HttpClient\CaClient;
+use SURFnet\VPN\Common\HttpClient\ServerClient;
+use SURFnet\VPN\Portal\Test\JsonTpl;
+use SURFnet\VPN\Portal\Test\TestHttpClient;
+use PHPUnit_Framework_TestCase;
+
+class VpnAdminModuleTest extends PHPUnit_Framework_TestCase
+{
+    /** @var \SURFnet\VPN\Common\Http\Service */
+    private $service;
+
+    public function setUp()
+    {
+        $httpClient = new TestHttpClient();
+
+        $this->service = new Service();
+        $this->service->addModule(
+            new AdminPortalModule(
+                new JsonTpl(),
+                new ServerClient($httpClient, 'serverClient'),
+                new CaClient($httpClient, 'caClient')
+            )
+        );
+        $this->service->addBeforeHook('auth', new NullAuthenticationHook('foo'));
+    }
+
+    public function testGetConnections()
+    {
+        $this->assertSame(
+            [
+                'vpnConnections' => [
+                    'idNameMapping' => [
+                        'internet' => 'Internet Access',
+                    ],
+                    'connections' => [
+                        'connections' => [
+                            [
+                                'bytes_in' => 5428,
+                                'bytes_out' => 5504,
+                                'common_name' => 'me_test',
+                                'connected_since' => 1474963126,
+                                'name' => 'test',
+                                'real_address' => '192.168.122.1:55461',
+                                'user_id' => 'me',
+                                'virtual_address' => [
+                                    '10.111.138.2',
+                                    'fd53:dbd:11bf:6460::1000',
+                                ],
+                            ],
+                        ],
+                        'id' => 'internet',
+                    ],
+                ],
+            ],
+            $this->makeRequest('GET', '/connections')
+        );
+    }
+
+    private function makeRequest($requestMethod, $pathInfo, array $getData = [], array $postData = [], $returnResponseObj = false)
+    {
+        $response = $this->service->run(
+            new Request(
+                [
+                    'SERVER_PORT' => 80,
+                    'SERVER_NAME' => 'vpn.example',
+                    'REQUEST_METHOD' => $requestMethod,
+                    'PATH_INFO' => $pathInfo,
+                    'REQUEST_URI' => $pathInfo,
+                ],
+                $getData,
+                $postData
+            )
+        );
+
+        if ($returnResponseObj) {
+            return $response;
+        }
+
+        $responseBody = $response->getBody();
+
+        return json_decode($responseBody, true);
+    }
+}
