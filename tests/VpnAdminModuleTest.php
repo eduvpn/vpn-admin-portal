c7f208efbc12379bddd71bb681b003c85a266398
François Kooman <fkooman@tuxed.net>
2016-12-15 10:45:46 +0100

    update dependency, fix for new path_info

--- tests/VpnAdminModuleTest.php
+++ tests/VpnAdminModuleTest.php
@@ -122,8 +122,8 @@ class VpnAdminModuleTest extends PHPUnit_Framework_TestCase
                     'SERVER_PORT' => 80,
                     'SERVER_NAME' => 'vpn.example',
                     'REQUEST_METHOD' => $requestMethod,
-                    'PATH_INFO' => sprintf('/%s', $pathInfo),
                     'REQUEST_URI' => sprintf('/%s', $pathInfo),
+                    'SCRIPT_NAME' => '/index.php',
                 ],
                 $getData,
                 $postData
