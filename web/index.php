54acb349d62e00f6ecd6572ac36c31786251144a
François Kooman <fkooman@tuxed.net>
2017-01-05 14:11:18 +0100

    remove NoCacheHook

--- web/index.php
+++ web/index.php
@@ -25,7 +25,6 @@ use SURFnet\VPN\Common\Http\FormAuthenticationHook;
 use SURFnet\VPN\Common\Http\FormAuthenticationModule;
 use SURFnet\VPN\Common\Http\HtmlResponse;
 use SURFnet\VPN\Common\Http\MellonAuthenticationHook;
-use SURFnet\VPN\Common\Http\NoCacheHook;
 use SURFnet\VPN\Common\Http\ReferrerCheckHook;
 use SURFnet\VPN\Common\Http\Request;
 use SURFnet\VPN\Common\Http\Service;
@@ -76,7 +75,6 @@ try {
 
     $service = new Service($tpl);
     $service->addBeforeHook('referrer_check', new ReferrerCheckHook());
-    $service->addAfterHook('no_cache', new NoCacheHook());
 
     // Authentication
     $authMethod = $config->getItem('authMethod');
