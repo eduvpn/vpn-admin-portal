38ea6276cff78e234b3e1e3dd253d14a1fdf7d8d
François Kooman <fkooman@tuxed.net>
2018-03-20 12:30:42 +0100

    stats can also be false

--- src/AdminPortalModule.php
+++ src/AdminPortalModule.php
@@ -213,7 +213,7 @@ class AdminPortalModule implements ServiceModuleInterface
             '/stats',
             function () {
                 $stats = $this->serverClient->get('stats');
-                if (!array_key_exists('profiles', $stats)) {
+                if (!is_array($stats) || !array_key_exists('profiles', $stats)) {
                     // this is an old "stats" format we no longer support,
                     // vpn-server-api-stats has to run again first, which is
                     // done by the crontab running at midnight...
